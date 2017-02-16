084cd46e87558edaf85547a1b9fc5b83cc3806ec
ADAM STONE <astone13@students.towson.edu>
2017-02-15 22:48:00 -0500

    Javadocs

--- src/edu/towson/cis/cosc442/project1/monopoly/Cell.java
+++ src/edu/towson/cis/cosc442/project1/monopoly/Cell.java
@@ -1,9 +1,19 @@
 package edu.towson.cis.cosc442.project1.monopoly;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Cell.
+ */
 public abstract class Cell implements IOwnable {
+	
+	/** The available. */
 	//Did pushdown
 	private boolean available = true;
+	
+	/** The name. */
 	private String name;
+	
+	/** The owner. */
 	protected Player theOwner;
 
 	/* (non-Javadoc)
@@ -52,6 +62,11 @@ public abstract class Cell implements IOwnable {
 		this.available = available;
 	}
 	
+	/**
+	 * Sets the name.
+	 *
+	 * @param name the new name
+	 */
 	void setName(String name) {
 		this.name = name;
 	}
